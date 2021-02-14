c89ba81f413f17bd30ab850993c42cf48e3d47a9
juhi-mehta992 <mehtajuhi.992@gmail.com>
2021-02-15 03:06:13 +0530

    second commit

--- src/shaadi/Marathishadi.java
+++ src/shaadi/Marathishadi.java
@@ -1,23 +1,44 @@
 package shaadi;
 
+import java.io.FileReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.opencsv.CSVReader;
+
+
+
 public class Marathishadi {
 
-	public static void main(String[] args) throws InterruptedException
+	
+	public static void main(String[] args) throws InterruptedException, Exception
 	{
 		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.marathishaadi.com/");
 		Thread.sleep(3000);
+			
+		 driver.findElement(By.xpath("//button[contains(text(),\"Let's Begin\")]")).click();
+		  
 		
-		driver.findElement(By.xpath("//button[contains(text(),\"Let's Begin\")]")).click();
-		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("juhi.1754@gmail.com");
-		driver.findElement(By.xpath("//input[@name ='password1']")).sendKeys("test1234");
+		//read data from csv file
+		CSVReader reader = new CSVReader(new FileReader("E:\\csvfile\\notepad.csv"));
+		String[] cell;
+		
+		
+		  while((cell=reader.readNext())!= null) 
+		  { 
+			  for(int i=0;i<1;i++) 
+			  { 
+				  String email= cell[i]; 
+				  String pwd = cell[i+1];	  		  
+				  driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+				  driver.findElement(By.xpath("//input[@name ='password1']")).sendKeys(pwd); 
+			  }
+		  
 		
 		WebElement profileFor = driver.findElement(By.xpath("//body/div[@id='___gatsby']/div[1]/div[7]/form[1]/div[2]/div[3]/div[1]/div[1]/div[1]"));
 		profileFor.click();
@@ -40,6 +61,7 @@ public class Marathishadi {
 			System.out.println("Community name is not correct");
 		}
 		driver.close();
+		}
+	
 	}
-
 }
